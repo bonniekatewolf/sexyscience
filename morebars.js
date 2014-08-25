a39df2dd2d6841cab395591250863b79cb329476
Bonnie Kate Wolf <bonniekatewolf@gmail.com>
2014-08-25 16:57:07 +0100

    Bars done for extension effect page, added JS and CSS

--- morebars.js
+++ morebars.js
@@ -3,11 +3,15 @@
 $(document).ready(function(e) {
 	
 	$('.short').click(calculate);
+	var extension_data = {short:20, long: 80,};
 	
 	function calculate () {
-		$('.redbar').removeClass('hide').removeClass('top');
-		$('.redbar').addClass('bottom');
-		$('.redbar').switchClass('bottom', 'top', 500);
+		$('.redbar').removeClass('hide').css('top', '99px').css('height', '1px');
+		$('.redbar.left').animate({'top': 100 - extension_data.short, height: extension_data.short,}, 500);
+		$('.redbar.right').animate({'top': 100 - extension_data.long, height: extension_data.long,}, 500);
+		$('.content').removeClass('hide');
+		
 	}
+	
 	
 });
