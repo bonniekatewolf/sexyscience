07dfbb7d5a8811940e1f55627ca77dc5598eee75
Bonnie Kate Wolf <bonniekatewolf@gmail.com>
2014-09-06 17:29:35 +0100

    Menu uses Mustache to make pages have links in different colours based on page

--- menu.js
+++ menu.js
@@ -3,7 +3,12 @@
 $(document).ready(function(e) {
 	
   $.get('menu.html', function(template) {
-    var rendered = Mustache.render(template, {name: "Menu"});
+  	var pagename = $('body').attr('id');
+  	
+  	var view = {};
+  	view[pagename] = true;
+  	
+    var rendered = Mustache.render(template, view);
     $('#menu').html(rendered);
   });
 	
