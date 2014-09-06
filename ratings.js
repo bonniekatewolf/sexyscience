848f26af85494f683e3a67706573ad981e1c30bf
Bonnie Kate Wolf <bonniekatewolf@gmail.com>
2014-09-06 19:45:36 +0100

    Ratings bars separately click to show

--- ratings.js
+++ ratings.js
@@ -1,24 +1,28 @@
- "use strict";
+"use strict";
 
 $(document).ready(function(e) {
 	$('.rating').click(rate);
-	
-	function rate () {
+
+	function rate() {
 		// 'this' is currently the bar that was clicked on
 		var clicked_percent = $(this).attr("data-rating");
-		
+		var clicked_percent_name = $(this).attr("data-name");
+
 		$('.rating').each(function() {
 			// 'this' is now going to be each rating bar in turn
-			
-			var this_percent = $(this).attr("data-rating");
-			if (this_percent <= clicked_percent) {
-				$(this).addClass('red');
-			}
-			else {
-				$(this).removeClass('red');
+
+			var base_percent = $(this).attr("data-rating");
+			var base_percent_name = $(this).attr("data-name");
+
+			if (base_percent_name == clicked_percent_name) {
+				if (base_percent <= clicked_percent) {
+					$(this).addClass('red');
+				} else {
+					$(this).removeClass('red');
+				}
 			}
 		});
-			
+
 	}
-	
-});
+
+}); 
