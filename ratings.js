fdd1ebd114695da3ba78332878a84a0092cbe512
Bonnie Kate Wolf <bonniekatewolf@gmail.com>
2014-09-06 20:08:32 +0100

    Rating bars now hold numbers and a number appears at the end

--- ratings.js
+++ ratings.js
@@ -3,10 +3,14 @@
 $(document).ready(function(e) {
 	$('.rating').click(rate);
 
+	var answers = {gracie: "none", zoe: "none", rachel: "none"};
+
 	function rate() {
 		// 'this' is currently the bar that was clicked on
 		var clicked_percent = $(this).attr("data-rating");
 		var clicked_percent_name = $(this).attr("data-name");
+
+		answers[clicked_percent_name] = clicked_percent;
 
 		$('.rating').each(function() {
 			// 'this' is now going to be each rating bar in turn
@@ -22,6 +26,20 @@ $(document).ready(function(e) {
 				}
 			}
 		});
+		
+		//it starts at 0, then after the clicks, if it is still at 0, it will remove the hide class
+
+		var not_selected = 0;
+		
+		for (var name in answers) {
+			if (answers[name] == "none") {
+				not_selected += 1;
+			}
+		}
+		
+		if (not_selected == 0) {
+			$('.number').removeClass('hide');
+		}
 
 	}
 
